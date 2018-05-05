3f2e7c5c37770d2a6fd705352944effff55a5570
FloPouyenne <flo.pouyenne@gmail.com>
2018-05-05 10:33:29 +0200

    1er commit

---
+++ 05-05atelier3/ex1/accueil.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<div>Bienvenue admin</div><div>Pour accéder à vos identifiants
+cliquez <a href='identifiant.php'>ici</a></div><div>Si vous souhaitez partir cliquez sur <a href='login.php'>déconnexion</a></div>";
+?>
